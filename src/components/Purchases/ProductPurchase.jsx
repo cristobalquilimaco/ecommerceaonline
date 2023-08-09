2eff8e88c835be1d347fa3a3e4895cd8c1bcdc51
cristobalquilimaco <quilimacox1@gmail.com>
2023-08-09 06:23:25 -0500

    Se aplican cambios de diseño en cart.css y ProductInCart

--- src/components/Purchases/ProductPurchase.jsx
+++ src/components/Purchases/ProductPurchase.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
+import "../Purchases/styles/productPurchase.css"
 
 const ProductPurchase = ({prodPurchase}) => {
   return (
-    <article>
-        <img src={prodPurchase.product.images[0].url} alt="" />
-        <h3>{prodPurchase.product.title}</h3>
-        <span>{prodPurchase.quantity}</span>
-        <span>{prodPurchase.quantity * prodPurchase.product.price}</span>
+    <div className='principal_purchase'>
+          <article className='product_purchase'>
+        <img className='img_purchase' src={prodPurchase.product.images[1].url} alt="" />
+        <h3 className='tt_purchase'>{prodPurchase.product.title}</h3>
+        <span className='quantity_purchase'>Purchased products {prodPurchase.quantity}</span>
+        <span className='quantity_purchase xs_quantity'>${prodPurchase.quantity * prodPurchase.product.price}</span>
     </article>
+    </div>
+
   )
 }
 
