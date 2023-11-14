9975ea6a09c8e0edd7b5d0cfa54fd62d54179c33
Cristobal <quilimacox1@gmail.com>
2023-11-13 19:15:45 -0500

    Actualiza codigo

--- src/components/banners/Banner.jsx
+++ src/components/banners/Banner.jsx
@@ -2,10 +2,10 @@
 import { useState } from "react";
 import images from "../../utils/img";
 import "./styles/Banner.css";
-import icon1 from "/public/cambios-devoluciones-icono.svg"
-import icon2 from "/public/envio-gratis.webp"
-import icon3 from "/public/gana-50.webp"
-import icon4 from "/public/venta-online.webp"
+import icon1 from "/public/cambios-devoluciones-icono.svg";
+import icon2 from "/public/envio-gratis.webp";
+import icon3 from "/public/gana-50.webp";
+import icon4 from "/public/venta-online.webp";
 
 const Banner = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
@@ -21,39 +21,45 @@ const Banner = () => {
   };
 
   return (
-
-          <div className="custom-banner">
-            <article className="custom__banner2">
-
-      <section className="custom-slider">
-        <div className="custom-slider-container" style={{ transform: `translateX(-${currentIndex * 100}%)` }}>
-          {images.map((image, index) => (
-            <div key={index} className="custom-slide">
+    <div className="custom-banner">
+      <article className="custom__banner2">
+        <section className="custom-slider">
+          <div className="custom-slider-container" style={{ transform: `translateX(-${currentIndex * 100}%)` }}>
+            {images.map((image, index) => (
+              <div key={index} className="custom-slide">
                 <img className="banner__img" src={image} alt="" />
-            </div>
-          ))}
-        </div>
-      </section>
-      <ul className="icon_info_banner">
-              <li className="icon__and_info"><img className="icon_individual" src={icon1} alt="" /><p>30 days for exchanges or returns.</p></li>
-              <li className="icon__and_info"><img className="icon_individual" src={icon2} alt="" /><p>Free shipping on all products to more than 930 municipalities.</p></li>
-              <li className="icon__and_info"><img className="icon_individual" src={icon3} alt="" /><p>You can earn half of your purchase.</p></li>
-              <li className="icon__and_info"><img className="icon_individual" src={icon4} alt="" /><p>Compra en línea y recoge en tienda.</p></li>
-            </ul>
-            <section className="custom-btn">
-        <button className="custom-prev-button" onClick={goToPrevIndex}>
-          Prev
-        </button>
-        <button className="custom-next-button" onClick={goToNextIndex}>
-          Next
-        </button>
-      </section>
-            </article>
-
+              </div>
+            ))}
+          </div>
+        </section>
+        <ul className="icon_info_banner">
+          <li className="icon__and_info">
+            <img className="icon_individual" src={icon1} alt="" />
+            <p>30 days for exchanges or returns.</p>
+          </li>
+          <li className="icon__and_info">
+            <img className="icon_individual" src={icon2} alt="" />
+            <p>Free shipping on all products to more than 930 municipalities.</p>
+          </li>
+          <li className="icon__and_info">
+            <img className="icon_individual" src={icon3} alt="" />
+            <p>You can earn half of your purchase.</p>
+          </li>
+          <li className="icon__and_info">
+            <img className="icon_individual" src={icon4} alt="" />
+            <p>Compra en línea y recoge en tienda.</p>
+          </li>
+        </ul>
+        <section className="custom-btn">
+          <button className="custom-prev-button" onClick={goToPrevIndex}>
+            Prev
+          </button>
+          <button className="custom-next-button" onClick={goToNextIndex}>
+            Next
+          </button>
+        </section>
+      </article>
     </div>
-    
-
-
   );
 };
 
