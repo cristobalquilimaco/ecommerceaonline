e421b1b630d097d5274c2b953165e3e018fa1f4a
Cristobal <quilimacox1@gmail.com>
2024-11-15 20:12:18 -0500

    Se ajusta componente header

--- src/components/shared/Header.jsx
+++ src/components/shared/Header.jsx
@@ -1,35 +1,9 @@
-import { useState, useEffect } from 'react';
+
 import { Link } from 'react-router-dom';
 import '../Home/styles/header.css';
-import FilterByPrice from '../Home/FilterByPrice';
-import FilterCategory from '../Home/FilterCategory';
 import imgstore from "/public/quilistore.png";
 
 const Header = ({ inputValue, onChangeInput }) => {
-    const [showFilter, setShowFilter] = useState(false);
-    const [showMenu, setShowMenu] = useState(false);
-
-    const handleClickToggleFilter = () => {
-        setShowFilter(!showFilter);
-    };
-
-    useEffect(() => {
-        const handleOutsideClick = (event) => {
-            if (
-                (showMenu && !event.target.classList.contains('list__icon__menu') && !event.target.classList.contains('bx-menu')) ||
-                (showFilter && !event.target.classList.contains('icon__filter') && !event.target.classList.contains('filter__menu'))
-            ) {
-                setShowMenu(false);
-                setShowFilter(false);
-            }
-        };
-
-        window.addEventListener('click', handleOutsideClick);
-
-        return () => {
-            window.removeEventListener('click', handleOutsideClick);
-        };
-    }, [showMenu, showFilter]);
 
     return (
         <header className='header__nav bg-light mb-4 bg-white'>
@@ -38,10 +12,10 @@ const Header = ({ inputValue, onChangeInput }) => {
                     <Link to='/'>
                         <img className='logo_store' src={imgstore} alt="Store Logo" />
                     </Link>
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded={showMenu} aria-label="Toggle navigation" onClick={() => setShowMenu(!showMenu)}>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
-                    <div className={`collapse navbar-collapse ${showMenu ? 'show' : ''}`} id="navbarNav">
+                    <div className="collapse navbar-collapse" id="navbarNav">
                         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                             <li className="nav-item">
                                 <Link to="/" className="nav-link active" aria-current="page">Home</Link>
@@ -58,11 +32,7 @@ const Header = ({ inputValue, onChangeInput }) => {
                             <li className="nav-item">
                                 <Link to="/cart" className="nav-link">Cart</Link>
                             </li>
-                            <li onClick={handleClickToggleFilter} className="nav-item nav__filter">
-                                <i className='bx bx-filter-alt icon__filter'></i>
-                            </li>
                         </ul>
-
                         <form className="d-flex" role="search" onSubmit={(e) => { e.preventDefault(); }}>
                             <input
                                 className="form-control me-2"
